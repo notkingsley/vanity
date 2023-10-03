0668438463ec44a10fdd02e9e693350f9b9cf952
kingsli <kingoyelabi@gmail.com>
2023-10-03 05:01:49 +0100

    add optional timeout to vanity::event::wait()

--- vanity/event.h
+++ vanity/event.h
@@ -41,11 +41,20 @@ public:
 		m_state = false;
 	}
 
-	// wait for the event to be set
+	// wait indefinitely for the event to be set
 	void wait() {
 		std::unique_lock<std::mutex> lock(m_mutex);
 		while (not is_set())
 			m_cond.wait(lock);
+	}
+
+	// wait for event or timeout in microseconds
+	// return false on timeout, true otherwise
+	bool wait(long timeout) {
+		using namespace std::chrono;
+		auto end = steady_clock::now() + microseconds{timeout};
+		std::unique_lock<std::mutex> lock {m_mutex};
+		return m_cond.wait_until(lock, end, [&](){return this->is_set();});
 	}
 };
 
