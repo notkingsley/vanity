238f811af399ce0e6638da87f394adfe9537e7d2
kingsli <kingoyelabi@gmail.com>
2023-10-11 23:46:57 +0100

    generalized extract<>() functions

--- vanity/request_server.cpp
+++ vanity/request_server.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <stdexcept>
+#include <tuple>
+
 #include "request_server.h"
 
 
@@ -15,27 +17,35 @@ enum class object_t{
 	FLOAT,
 };
 
-// the concrete type of object_t
-template <object_t>
-struct concrete;
+template<object_t _obj>
+struct concrete_traits;
 
-template <>
-struct concrete<object_t::STR> {
+template<>
+struct concrete_traits<object_t::STR> {
 	using type = std::string;
 };
 
-template <>
-struct concrete<object_t::INT> {
+template<>
+struct concrete_traits<object_t::INT> {
 	using type = int64_t;
 };
 
-template <>
-struct concrete<object_t::FLOAT> {
+template<>
+struct concrete_traits<object_t::FLOAT> {
 	using type = double;
 };
 
+template <object_t ...Args> struct concrete {
+	using type = std::tuple<typename concrete_traits<Args>::type...>;
+};
+
 template<object_t _obj>
-using concrete_t = typename concrete<_obj>::type;
+struct concrete<_obj> {
+	using type = typename concrete_traits<_obj>::type;
+};
+
+template<object_t ...Args>
+using concrete_t = typename concrete<Args...>::type;
 
 // increment pos until it is not a whitespace
 static inline void skip_whitespace(const std::string& msg, size_t& pos)
@@ -136,8 +146,21 @@ static inline client_auth extract_client_auth(const std::string& msg, size_t& po
 }
 
 // extract an object from part of a message
-template<object_t obj>
-inline concrete_t<obj> extract(const std::string& msg, size_t& pos);
+template<object_t ...Args>
+static inline concrete_t<Args...> extract(const std::string& msg, size_t& pos)
+{
+	return {extract<Args>(msg, pos)...};
+}
+
+// extract exactly the given object types from the rest of a message
+// throw if there are more or less than the given object types
+template<object_t ...Args>
+static inline concrete_t<Args...> extract_exact(const std::string& msg, size_t& pos)
+{
+	auto ret = extract<Args...>(msg, pos);
+	ensure_end(msg, pos);
+	return ret;
+}
 
 // extract an int64_t from part of a message
 template<>
@@ -204,34 +227,6 @@ inline std::string extract<object_t::STR>(const std::string& msg, size_t& pos)
 	throw InvalidRequest("word not closed with quotes");
 }
 
-// extract exactly a single string from the rest of a message
-// throw if there is more than one string
-static inline std::string extract_single_string(const std::string& msg, size_t& pos)
-{
-	std::string word = extract<object_t::STR>(msg, pos);
-	ensure_end(msg, pos);
-	return word;
-}
-
-// extract exactly two strings from the rest of a message
-// throw if there are more than two strings
-static inline std::pair<std::string, std::string> extract_two_strings(const std::string& msg, size_t& pos)
-{
-	std::string word1 = extract<object_t::STR>(msg, pos);
-	std::string word2 = extract<object_t::STR>(msg, pos);
-	ensure_end(msg, pos);
-	return {word1, word2};
-}
-
-// extract exactly a single int64_t from the rest of a message
-// throw if there is more than one int64_t
-static inline int64_t extract_single_int(const std::string& msg, size_t& pos)
-{
-	int64_t value = extract<object_t::INT>(msg, pos);
-	ensure_end(msg, pos);
-	return value;
-}
-
 std::string RequestServer::prepare(const std::string &msg) {
 	std::string ret {};
 	ret.reserve(msg.size() + 10);
@@ -257,10 +252,11 @@ void RequestServer::handle(const std::string& msg, const Client& client) {
 		if (not client.has_perm(op))
 			return send(client, server_constants::denied);
 
+		using object_t::STR, object_t::INT, object_t::FLOAT;
 		switch (op) {
 			case operation_t::GET:
 			{
-				request_get(client, extract_single_string(msg, pos));
+				request_get(client, extract_exact<STR>(msg, pos));
 				break;
 			}
 			case operation_t::SET:
@@ -270,23 +266,23 @@ void RequestServer::handle(const std::string& msg, const Client& client) {
 			}
 			case operation_t::DEL:
 			{
-				request_del(client, extract_single_string(msg, pos));
+				request_del(client, extract_exact<STR>(msg, pos));
 				break;
 			}
 			case operation_t::SWITCH_DB:
 			{
-				request_switch_db(client, extract_single_int(msg, pos));
+				request_switch_db(client, extract_exact<INT>(msg, pos));
 				break;
 			}
 			case operation_t::ADD_USER:
 			{
-				auto [username, password] = extract_two_strings(msg, pos);
+				auto [username, password] = extract_exact<STR, STR>(msg, pos);
 				request_add_user(client, username, password);
 				break;
 			}
 			case operation_t::EDIT_USER:
 			{
-				auto username = extract<object_t::STR>(msg, pos);
+				auto username = extract<STR>(msg, pos);
 				auto auth = extract_client_auth(msg, pos);
 				ensure_end(msg, pos);
 				request_edit_user(client, username, auth);
@@ -294,18 +290,18 @@ void RequestServer::handle(const std::string& msg, const Client& client) {
 			}
 			case operation_t::DEL_USER:
 			{
-				request_del_user(client, extract_single_string(msg, pos));
+				request_del_user(client, extract_exact<STR>(msg, pos));
 				break;
 			}
 			case operation_t::AUTH:
 			{
-				auto [username, password] = extract_two_strings(msg, pos);
+				auto [username, password] = extract_exact<STR, STR>(msg, pos);
 				request_auth(client, username, password);
 				break;
 			}
 			case operation_t::CHANGE_PASSWORD:
 			{
-				request_change_password(client, extract_single_string(msg, pos));
+				request_change_password(client, extract_exact<STR>(msg, pos));
 				break;
 			}
 			case operation_t::PERSIST:
@@ -348,24 +344,24 @@ void RequestServer::handle(const std::string& msg, const Client& client) {
 }
 
 void RequestServer::dispatch_set(const Client &client, const std::string &msg, size_t &pos) {
+	using object_t::STR, object_t::INT, object_t::FLOAT;
+
 	object_t obj = extract_object_t(msg, pos);
-	std::string key = extract<object_t::STR>(msg, pos);
+	std::string key = extract<STR>(msg, pos);
+
 	switch (obj) {
-		case object_t::STR:{
-			auto value {extract<object_t::STR>(msg, pos)};
-			ensure_end(msg, pos);
+		case STR:{
+			auto value {extract_exact<STR>(msg, pos)};
 			request_set(client, key, value);
 			break;
 		}
-		case object_t::INT:{
-			auto value {extract<object_t::INT>(msg, pos)};
-			ensure_end(msg, pos);
+		case INT:{
+			auto value {extract_exact<INT>(msg, pos)};
 			request_set(client, key, value);
 			break;
 		}
-		case object_t::FLOAT:{
-			auto value {extract<object_t::FLOAT>(msg, pos)};
-			ensure_end(msg, pos);
+		case FLOAT:{
+			auto value {extract_exact<FLOAT>(msg, pos)};
 			request_set(client, key, value);
 			break;
 		}
