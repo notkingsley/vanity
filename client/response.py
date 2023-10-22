3b0f700555a80b064a78b7fc886bbff402c899b7
kingsli <kingoyelabi@gmail.com>
2023-10-22 19:55:24 +0100

    update client response parsing

--- client/response.py
+++ client/response.py
@@ -19,42 +19,81 @@ class InvalidResponseError(Exception):
 	pass
 
 
-def extract(msg: str) -> str | int | float | None:
+def extract_status(msg: str) -> tuple[ServerConstant | None, str]:
 	"""
-	Extract a value from a response.
+	Extract the status of a response, if any.
+	Return the status, and the remaining message.
+	"""
+	for constant in ServerConstant:
+		if msg.startswith(constant.value):
+			return constant, msg[len(constant.value):].strip()
+	
+	return None, msg
+
+
+def extract_str(msg: str) -> str | None:
+	"""
+	Extract a string from a response.
 	:param msg: The response to extract from.
-	:return: The extracted value, or None if no value could be deciphered.
+	:return: The extracted string, or None if no string could be deciphered.
 	"""
-	if msg.startswith(":"):
-		msg = msg[1:].strip()
-		
-		if msg.startswith("STR"):
-			msg = msg[3:].strip()
-			if msg.startswith('"') and msg.endswith('"'):
-				return msg[1:-1].replace(r'\"', '"')
-		
-		elif msg.startswith("INT"):
-			msg = msg[3:].strip()
-			if msg.isdigit():
-				return int(msg)
-		
-		elif msg.startswith("FLOAT"):
-			msg = msg[5:].strip()
-			if msg.replace(".", "", 1).isdigit():
-				return float(msg)
+	if msg.startswith('"') and msg.endswith('"'):
+		return msg[1:-1].replace(r'\"', '"')
 	
 	return None
+
+def extract_as(msg: str, type: type[int | float | str]):
+	"""
+	Extract a value from a response as a given type.
+	:param msg: The response to extract from.
+	:param type: The type to extract as.
+	:return: The extracted value, or None if no value could be deciphered.
+	"""
+	if type is str:
+		return extract_str(msg)
+	
+	if type is int:
+		return int(msg) if msg.isdigit() else None
+	
+	if type is float:
+		return float(msg) if msg.replace(".", "", 1).isdigit() else None
+	
+
+def extract_type(msg: str) -> tuple[type[str | int | float] | None, str]:
+	"""
+	Extract the type of a response, if any.
+	Return the type, and the remaining message.
+	"""
+	if msg.startswith(":STR"):
+		return str, msg[4:].strip()
+	
+	elif msg.startswith(":INT"):
+		return int, msg[4:].strip()
+	
+	elif msg.startswith(":FLOAT"):
+		return float, msg[6:].strip()
+
+	return None, msg
+
 
 class Response:
 	def __init__(self, body: str) -> None:
 		self._raw = body.strip()
-		self._parse()
+
+		raw = self._raw
+		self.status, raw = extract_status(raw)
+		self.type, raw = extract_type(raw)
+
+		self.value = None
+		if self.type:
+			self.value = extract_as(raw, self.type)
+			return
 
 	def __str__(self) -> str:
 		status = self.status.name if self.status else self.status
-		body = self.body
-		if self.body and len(self.body) > 30:
-			body = self.body[:30] + "..."
+		body = self._raw
+		if self._raw and len(self._raw) > 30:
+			body = self._raw[:30] + "..."
 
 		return f"<Response: status={status}, body={body}>"
 
@@ -62,23 +101,7 @@ class Response:
 		"""
 		Parse the raw response.
 		"""
-		self.status = None
-		self.body = None
-		raw = self._raw
 
-		for constant in ServerConstant:
-			if raw.startswith(constant.value):
-				self.status = constant
-				raw = raw[len(constant.value):].strip()
-				break
-		
-		if raw:
-			self.body = raw
-			self.value = extract(self.body)
-
-		if self.status is None and self.body is None:
-			raise InvalidResponseError("Response is empty.")
-	
 	def is_null(self) -> bool:
 		"""
 		:return: True if the response is NULL, False otherwise.
