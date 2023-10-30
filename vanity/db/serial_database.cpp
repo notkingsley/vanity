02549c65dcd0e86a2d1dccc4c87dbe7679d03816
kingsli <kingoyelabi@gmail.com>
2023-10-30 23:33:29 +0100

    add list_ methods to SerialDatabase

--- vanity/db/serial_database.cpp
+++ vanity/db/serial_database.cpp
@@ -2,6 +2,9 @@
 // Created by kingsli on 10/20/23.
 //
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "HidingNonVirtualFunction"
+
 #include "serial_database.h"
 
 namespace vanity::db {
@@ -15,79 +18,139 @@ SerialDatabase &SerialDatabase::operator=(Database &&other) noexcept {
 
 void SerialDatabase::perform(task_type task, task_data_type data, std::promise<ret_type> promise) {
 	switch (task) {
-		case task_type::GET:
+		case GET:
 		{
 			auto& key = std::get<get_type>(data);
 			promise.set_value(Database::get(key));
 			break;
 		}
-		case task_type::SET:
+		case SET:
 		{
 			auto& [key, value] = std::get<set_type>(data);
 			Database::set(key, value);
 			promise.set_value(std::monostate{});
 			break;
 		}
-		case task_type::DEL:
+		case DEL:
 		{
 			auto& key = std::get<del_type>(data);
 			promise.set_value(Database::del(key));
 			break;
 		}
-		case task_type::TYPE:
+		case TYPE:
 		{
 			auto& key = std::get<type_type>(data);
 			promise.set_value(Database::type(key));
 			break;
 		}
-		case task_type::HAS:
+		case HAS:
 		{
 			auto& key = std::get<has_type>(data);
 			promise.set_value(Database::has(key));
 			break;
 		}
-		case task_type::INCR_INT:
+		case INCR_INT:
 		{
 			auto& [key, value] = std::get<incr_int_type>(data);
 			promise.set_value(Database::incr_int(key, value));
 			break;
 		}
-		case task_type::INCR_FLOAT:
+		case INCR_FLOAT:
 		{
 			auto& [key, value] = std::get<incr_float_type>(data);
 			promise.set_value(Database::incr_float(key, value));
 			break;
 		}
-		case task_type::STR_LEN:
+		case STR_LEN:
 		{
 			auto& key = std::get<str_len_type>(data);
 			promise.set_value(Database::str_len(key));
 			break;
 		}
-		case task_type::RESET:
+		case RESET:
 		{
 			Database::reset();
 			promise.set_value(std::monostate{});
 			break;
 		}
-		case task_type::PERSIST:
+		case PERSIST:
 		{
 			auto& [out] = std::get<persist_type>(data);
 			Database::persist(out);
 			promise.set_value(std::monostate{});
 			break;
 		}
-		case task_type::MANY_GET:
+		case MANY_GET:
 		{
 			auto& keys = std::get<many_get_type>(data);
 			promise.set_value(Database::many_get(keys));
 			break;
 		}
-		case task_type::MANY_SET:
+		case MANY_SET:
 		{
 			auto& keys = std::get<many_set_type>(data);
 			Database::many_set(std::move(keys));
 			promise.set_value(std::monostate{});
+			break;
+		}
+		case LIST_LEN:
+		{
+			auto& key = std::get<list_len_type>(data);
+			promise.set_value(Database::list_len(key));
+			break;
+		}
+		case LIST_GET:
+		{
+			auto& [key, index] = std::get<list_get_type>(data);
+			promise.set_value(Database::list_get(key, index));
+			break;
+		}
+		case LIST_SET:
+		{
+			auto& [key, index, value] = std::get<list_set_type>(data);
+			promise.set_value(Database::list_set(key, index, value));
+			break;
+		}
+		case LIST_PUSH_LEFT:
+		{
+			auto& [key, values] = std::get<list_push_left_type>(data);
+			promise.set_value(Database::list_push_left(key, std::move(values)));
+			break;
+		}
+		case LIST_PUSH_RIGHT:
+		{
+			auto& [key, values] = std::get<list_push_right_type>(data);
+			promise.set_value(Database::list_push_right(key, std::move(values)));
+			break;
+		}
+		case LIST_POP_LEFT:
+		{
+			auto& [key, n] = std::get<list_pop_left_type>(data);
+			promise.set_value(Database::list_pop_left(key, n));
+			break;
+		}
+		case LIST_POP_RIGHT:
+		{
+			auto& [key, n] = std::get<list_pop_right_type>(data);
+			promise.set_value(Database::list_pop_right(key, n));
+			break;
+		}
+		case LIST_RANGE:
+		{
+			auto& [key, start, stop] = std::get<list_range_type>(data);
+			promise.set_value(Database::list_range(key, start, stop));
+			break;
+		}
+		case LIST_TRIM:
+		{
+			auto& [key, start, stop] = std::get<list_trim_type>(data);
+			promise.set_value(Database::list_trim(key, start, stop));
+			break;
+		}
+		case LIST_REMOVE:
+		{
+			auto& [key, value, count] = std::get<list_remove_type>(data);
+			promise.set_value(Database::list_remove(key, value, count));
 			break;
 		}
 	}
@@ -105,52 +168,97 @@ void SerialDatabase::stop() {
 	TaskSerializer::stop();
 }
 
+
 void SerialDatabase::persist(std::ofstream &out) {
-	send_task(task_type::PERSIST, out).wait();
+	send_task(PERSIST, out).wait();
 }
 
 void SerialDatabase::reset() {
-	send_task(task_type::RESET, std::monostate{}).wait();
+	send_task(RESET, std::monostate{}).wait();
 }
 
-bool SerialDatabase::has(const Database::key_type &key) {
-	return std::get<has_ret_type>(send_task(task_type::HAS, key).get());
+bool SerialDatabase::has(const key_type &key) {
+	return std::get<has_ret_type>(send_task(HAS, key).get());
 }
 
-std::optional<Database::data_type> SerialDatabase::get(const Database::key_type &key) {
-	return std::get<get_ret_type>(send_task(task_type::GET, key).get());
+bool SerialDatabase::del(const key_type &key) {
+	return std::get<del_ret_type>(send_task(DEL, key).get());
 }
 
-void SerialDatabase::set(const Database::key_type &key, const Database::data_type &value) {
-	send_task(task_type::SET, std::make_tuple(key, value)).wait();
+
+auto SerialDatabase::get(const key_type &key) -> get_ret_type {
+	return std::get<get_ret_type>(send_task(GET, key).get());
 }
 
-bool SerialDatabase::del(const Database::key_type &key) {
-	return std::get<del_ret_type>(send_task(task_type::DEL, key).get());
+void SerialDatabase::set(const key_type &key, const data_type& value) {
+	send_task(SET, std::make_tuple(key, value)).wait();
 }
 
-std::optional<int> SerialDatabase::type(const Database::key_type &key) {
-	return std::get<type_ret_type>(send_task(task_type::TYPE, key).get());
+auto SerialDatabase::type(const key_type &key) -> type_ret_type {
+	return std::get<type_ret_type>(send_task(TYPE, key).get());
 }
 
-std::optional<int_t> SerialDatabase::incr_int(const Database::key_type &key, int_t value) {
-	return std::get<incr_int_ret_type>(send_task(task_type::INCR_INT, std::make_tuple(key, value)).get());
+auto SerialDatabase::incr_int(const key_type &key, int_t value) -> incr_int_ret_type{
+	return std::get<incr_int_ret_type>(send_task(INCR_INT, std::make_tuple(key, value)).get());
 }
 
-std::optional<float_t> SerialDatabase::incr_float(const Database::key_type &key, float_t value) {
-	return std::get<incr_float_ret_type>(send_task(task_type::INCR_FLOAT, std::make_tuple(key, value)).get());
+auto SerialDatabase::incr_float(const key_type &key, float_t value) -> incr_float_ret_type {
+	return std::get<incr_float_ret_type>(send_task(INCR_FLOAT, std::make_tuple(key, value)).get());
 }
 
-std::optional<int_t> SerialDatabase::str_len(const key_type &key) {
-	return std::get<str_len_ret_type>(send_task(task_type::STR_LEN, key).get());
+auto SerialDatabase::str_len(const key_type &key) -> str_len_ret_type {
+	return std::get<str_len_ret_type>(send_task(STR_LEN, key).get());
 }
 
-std::vector<std::optional<Database::data_type>> SerialDatabase::many_get(const std::vector<key_type> &keys) {
-	return std::move(std::move(std::get<many_get_ret_type>(send_task(task_type::MANY_GET, keys).get())));
+auto SerialDatabase::many_get(const std::vector<key_type> &keys) -> many_get_ret_type {
+	return std::move(std::move(std::get<many_get_ret_type>(send_task(MANY_GET, keys).get())));
 }
 
 void SerialDatabase::many_set(std::vector<std::pair<db_key_type, db_data_type>> keys) {
-	send_task(task_type::MANY_SET, std::move(keys)).wait();
+	send_task(MANY_SET, std::move(keys)).wait();
+}
+
+
+auto SerialDatabase::list_len(const key_type &key) -> list_len_ret_type{
+	return std::get<list_len_ret_type>(send_task(LIST_LEN, key).get());
+}
+
+auto SerialDatabase::list_get(const key_type &key, int index) -> list_get_ret_type {
+	return std::get<list_get_ret_type>(send_task(LIST_GET, std::make_tuple(key, index)).get());
+}
+
+auto SerialDatabase::list_set(const key_type &key, int index, const std::string &value) -> list_set_ret_type {
+	return std::get<list_set_ret_type>(send_task(LIST_SET, std::make_tuple(key, index, value)).get());
+}
+
+auto SerialDatabase::list_push_left(const key_type &key, list_t values) -> list_push_left_ret_type {
+	return std::get<list_push_left_ret_type>(send_task(LIST_PUSH_LEFT, std::make_tuple(key, std::move(values))).get());
+}
+
+auto SerialDatabase::list_push_right(const key_type &key, list_t values) -> list_push_right_ret_type {
+	return std::get<list_push_right_ret_type>(send_task(LIST_PUSH_RIGHT, std::make_tuple(key, std::move(values))).get());
+}
+
+auto SerialDatabase::list_pop_left(const key_type &key, int n) -> list_pop_left_ret_type {
+	return std::get<list_pop_left_ret_type>(send_task(LIST_POP_LEFT, std::make_tuple(key, n)).get());
+}
+
+auto SerialDatabase::list_pop_right(const key_type &key, int n) -> list_pop_right_ret_type {
+	return std::get<list_pop_right_ret_type>(send_task(LIST_POP_RIGHT, std::make_tuple(key, n)).get());
+}
+
+auto SerialDatabase::list_range(const key_type &key, int start, int stop) -> list_range_ret_type {
+	return std::get<list_range_ret_type>(send_task(LIST_RANGE, std::make_tuple(key, start, stop)).get());
+}
+
+auto SerialDatabase::list_trim(const key_type &key, int start, int stop) -> list_trim_ret_type {
+	return std::get<list_trim_ret_type>(send_task(LIST_TRIM, std::make_tuple(key, start, stop)).get());
+}
+
+auto SerialDatabase::list_remove(const key_type &key, const std::string &value, int count) -> list_remove_ret_type {
+	return std::get<list_remove_ret_type>(send_task(LIST_REMOVE, std::make_tuple(key, value, count)).get());
 }
 
 } // namespace vanity::db
+
+#pragma clang diagnostic pop
--- vanity/db/serial_database.h
+++ vanity/db/serial_database.h
@@ -2,6 +2,9 @@
 // Created by kingsli on 10/20/23.
 //
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "HidingNonVirtualFunction"
+
 #ifndef VANITY_SERIAL_DATABASE_H
 #define VANITY_SERIAL_DATABASE_H
 
@@ -13,67 +16,130 @@ namespace vanity::db {
 
 // define the types used by the serial database
 struct serial_database_types {
-	enum class task_type {
-		GET,
-		SET,
+private:
+	using key_type = Database::key_type;
+	using data_type = Database::data_type;
+
+protected:
+	// parameter types
+	// base database
+	using reset_type = void;
+	using del_type = key_type;
+	using has_type = key_type;
+	using type_type = key_type;
+	using persist_type = std::tuple<std::ofstream&>;
+
+	// primitive database
+	using get_type = key_type;
+	using set_type = std::tuple<key_type, data_type>;
+	using incr_int_type = std::tuple<key_type, int_t>;
+	using incr_float_type = std::tuple<key_type, float_t>;
+	using str_len_type = key_type;
+	using many_get_type = std::vector<key_type>;
+	using many_set_type = std::vector<std::pair<key_type, data_type>>;
+
+	// list database
+	using list_len_type = key_type;
+	using list_get_type = std::tuple<key_type, int>;
+	using list_set_type = std::tuple<key_type, int, std::string>;
+	using list_push_left_type = std::tuple<key_type, list_t>;
+	using list_push_right_type = std::tuple<key_type, list_t>;
+	using list_pop_left_type = std::tuple<key_type, int>;
+	using list_pop_right_type = std::tuple<key_type, int>;
+	using list_range_type = std::tuple<key_type, int, int>;
+	using list_trim_type = std::tuple<key_type, int, int>;
+	using list_remove_type = std::tuple<key_type, std::string, int>;
+
+
+	// return types
+	// base database
+	using has_ret_type = bool;
+	using del_ret_type = bool;
+	using reset_ret_type = void;
+	using type_ret_type = std::optional<int>;
+	using persist_ret_type = void;
+
+	// primitive database
+	using get_ret_type = std::optional<data_type>;
+	using set_ret_type = void;
+	using incr_int_ret_type = std::optional<int_t>;
+	using incr_float_ret_type = std::optional<float_t>;
+	using str_len_ret_type = std::optional<int_t>;
+	using many_get_ret_type = std::vector<std::optional<data_type>>;
+	using many_set_ret_type = void;
+
+	// list database
+	using list_len_ret_type = std::variant<size_t, ErrorKind>;
+	using list_get_ret_type = std::variant<std::string, ErrorKind>;
+	using list_set_ret_type = std::variant<bool, ErrorKind>;
+	using list_push_left_ret_type = std::variant<size_t, ErrorKind>;
+	using list_push_right_ret_type = std::variant<size_t, ErrorKind>;
+	using list_pop_left_ret_type = std::variant<list_t, ErrorKind>;
+	using list_pop_right_ret_type = std::variant<list_t, ErrorKind>;
+	using list_range_ret_type = std::variant<list_t, ErrorKind>;
+	using list_trim_ret_type = std::variant<size_t, ErrorKind>;
+	using list_remove_ret_type = std::variant<size_t, ErrorKind>;
+
+public:
+	enum task_type {
+		RESET,
+		PERSIST,
 		DEL,
 		TYPE,
+
+		GET,
+		SET,
 		HAS,
 		INCR_INT,
 		INCR_FLOAT,
 		STR_LEN,
 		MANY_GET,
 		MANY_SET,
-		RESET,
-		PERSIST,
+
+		LIST_LEN,
+		LIST_GET,
+		LIST_SET,
+		LIST_PUSH_LEFT,
+		LIST_PUSH_RIGHT,
+		LIST_POP_LEFT,
+		LIST_POP_RIGHT,
+		LIST_RANGE,
+		LIST_TRIM,
+		LIST_REMOVE,
 	};
 
-	// parameter types
-	using get_type = Database::key_type;
-	using set_type = std::tuple<Database::key_type, Database::data_type>;
-	using del_type = Database::key_type;
-	using has_type = Database::key_type;
-	using type_type = Database::key_type;
-	using incr_int_type = std::tuple<Database::key_type, int_t>;
-	using incr_float_type = std::tuple<Database::key_type, float_t>;
-	using str_len_type = Database::key_type;
-	using many_get_type = std::vector<Database::key_type>;
-	using many_set_type = std::vector<std::pair<Database::key_type, Database::data_type>>;
-	using reset_type = void;
-	using persist_type = std::tuple<std::ofstream&>;
-
-	// return types
-	using get_ret_type = std::optional<Database::data_type>;
-	using set_ret_type = void;
-	using del_ret_type = bool;
-	using has_ret_type = bool;
-	using type_ret_type = std::optional<int>;
-	using incr_int_ret_type = std::optional<int_t>;
-	using incr_float_ret_type = std::optional<float_t>;
-	using str_len_ret_type = std::optional<int_t>;
-	using many_get_ret_type = std::vector<std::optional<Database::data_type>>;
-	using many_set_ret_type = void;
-	using reset_ret_type = void;
-	using persist_ret_type = void;
-
-
 	using task_data_type = std::variant<
+		persist_type,
+
 		get_type,
 		set_type,
 		incr_int_type,
 		incr_float_type,
 		many_get_type,
 		many_set_type,
-		persist_type,
+
+		list_set_type,
+		list_push_left_type,
+		list_pop_left_type,
+		list_range_type,
+		list_remove_type,
+
 		std::monostate
 	>;
 	using ret_type = std::variant<
-		get_ret_type,
 		del_ret_type,
 		type_ret_type,
+
+		get_ret_type,
 		incr_int_ret_type,
 		incr_float_ret_type,
 		many_get_ret_type,
+
+		list_len_ret_type,
+		list_get_ret_type,
+		list_set_ret_type,
+		list_pop_left_ret_type,
+
 		std::monostate
 	>;
 };
@@ -82,7 +148,7 @@ struct serial_database_types {
  * A SerialDatabase serializes all database operations to a single thread
  */
 class SerialDatabase :
-	public virtual Database,
+	public Database,
 	private serial_database_types,
 	private TaskSerializer<
 		serial_database_types::task_type,
@@ -92,6 +158,8 @@ class SerialDatabase :
 {
 private:
 	using serial_database_types::task_data_type;
+	using Database::key_type;
+	using Database::data_type;
 
 	// perform a task that was sent from another thread
 	void perform(task_type task, task_data_type data, std::promise<ret_type> promise) override;
@@ -119,6 +187,7 @@ public:
 	// stop the database's operation thread
 	void stop();
 
+
 	// persist the database to a file stream
 	void persist(std::ofstream &out);
 
@@ -128,25 +197,26 @@ public:
 	// check if the key exists
 	bool has(const key_type& key);
 
+	// delete the value for a key
+	bool del(const key_type& key);
+
+
 	// get the value for a key
-	std::optional<Database::data_type> get(const key_type& key);
+	std::optional<data_type> get(const key_type& key);
 
 	// set the value for a key
 	void set(const key_type& key, const db_data_type& value);
-
-	// delete the value for a key
-	bool del(const key_type& key);
 
 	// get the type of key as an index
 	std::optional<int> type(const key_type& key);
 
 	// increment the value for an integer key, or set it to 0 if it doesn't exist
 	// returns the new value, or std::nullopt if the value is not an integer
-	std::optional<int_t> incr_int(const Database::key_type &key, int_t value);
+	std::optional<int_t> incr_int(const key_type &key, int_t value);
 
 	// increment the value for a float key, or set it to 0 if it doesn't exist
 	// returns the new value, or std::nullopt if the value is not a float
-	std::optional<float_t> incr_float(const Database::key_type &key, float_t value);
+	std::optional<float_t> incr_float(const key_type &key, float_t value);
 
 	// get the length of a string key
 	// returns the length, or std::nullopt
@@ -154,12 +224,67 @@ public:
 	std::optional<int_t> str_len(const key_type &key);
 
 	// get the value for many keys
-	std::vector<std::optional<Database::data_type>> many_get(const std::vector<key_type>& keys);
+	std::vector<std::optional<data_type>> many_get(const std::vector<key_type>& keys);
 
 	// set the value for many keys
 	void many_set(std::vector<std::pair<db_key_type, db_data_type>> keys);
+
+
+	// get the length of a list key
+	// returns the length, 0 if value does not exist or is empty,
+	// or ErrorKind::NotList if the value exists and is not a list
+	std::variant<size_t, ErrorKind> list_len(const key_type &key);
+
+	// get the value for a list key at a given index
+	// returns the value, or ErrorKind::NotList if the value is not a list
+	// or ErrorKind::OutOfRange if the index is out of range or if value does not exist
+	std::variant<std::string, ErrorKind> list_get(const key_type &key, int index);
+
+	// set the value for a list key at a given index
+	// returns true if the value was set, or ErrorKind::NotList if the value is not a list
+	// or ErrorKind::OutOfRange if the index is out of range or if value does not exist
+	std::variant<bool, ErrorKind> list_set(const key_type &key, int index, const std::string& value);
+
+	// push values to the top of a list
+	// returns the new length, or ErrorKind::NotList if the value is not a list
+	std::variant<size_t, ErrorKind> list_push_left(const key_type &key, list_t values);
+
+	// push values to the bottom of a list
+	// returns the new length, or ErrorKind::NotList if the value is not a list
+	std::variant<size_t, ErrorKind> list_push_right(const key_type &key, list_t values);
+
+	// pop n value from the top of a list
+	// returns the values or ErrorKind::NotList if the value is not a list
+	// if the index is positive and out of range, returns the values up to the end of the list
+	// if the index is negative and out of range, returns an empty list
+	std::variant<list_t, ErrorKind> list_pop_left(const key_type &key, int n);
+
+	// pop n values from the bottom of a list
+	// returns the values, or ErrorKind::NotList if the value is not a list
+	// if the index is positive and out of range, returns the values up to the end of the list
+	// if the index is negative and out of range, returns an empty list
+	std::variant<list_t, ErrorKind> list_pop_right(const key_type &key, int n);
+
+	// get the value for a range of a list key inclusively
+	// returns the values, or ErrorKind::NotList if the value is not a list
+	// returns an empty list if the range is out of bounds or invalid
+	std::variant<list_t, ErrorKind> list_range(const key_type &key, int start, int end);
+
+	// trim the list stored at key, so that it will
+	// contain only the specified range of elements (inclusively)
+	// returns the number of trimmed elements
+	// or ErrorKind::NotList if the value is not a list
+	std::variant<size_t, ErrorKind> list_trim(const key_type &key, int start, int end);
+
+	// remove a number of elements equal to count from the list stored at key
+	// that hold the value element
+	// returns the number of removed elements, or ErrorKind::NotList if the value is not a list
+	// removes from the end if count is negative or all elements if count is 0
+	std::variant<size_t, ErrorKind> list_remove(const key_type &key, const std::string& element, int count);
 };
 
 } // namespace vanity::db
 
 #endif //VANITY_SERIAL_DATABASE_H
+
+#pragma clang diagnostic pop
