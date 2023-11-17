5b113cc20fb9d7cf96b76bbabaca8b13476d80a9
kingsli <kingoyelabi@gmail.com>
2023-11-18 00:15:02 +0100

    disallow auth related commands in PIPE

--- vanity/pipe_server.cpp
+++ vanity/pipe_server.cpp
@@ -21,4 +21,34 @@ void PipeServer::request_pipe(Client &client, const std::string &msg, size_t &po
 	piped_client.perform_write(*this);
 }
 
+void PipeServer::request_add_user(Client &client, const std::string &username, const std::string &password) {
+	if (dynamic_cast<PipedClient*>(&client))
+		return send(client, error("ADD_USER command not allowed in PIPE request"));
+	SerialAuthServer::request_add_user(client, username, password);
+}
+
+void PipeServer::request_edit_user(Client &client, const std::string &username, client_auth auth_level) {
+	if (dynamic_cast<PipedClient*>(&client))
+		return send(client, error("EDIT_USER command not allowed in PIPE request"));
+	SerialAuthServer::request_edit_user(client, username, auth_level);
+}
+
+void PipeServer::request_del_user(Client &client, const std::string &username) {
+	if (dynamic_cast<PipedClient*>(&client))
+		return send(client, error("DEL_USER command not allowed in PIPE request"));
+	SerialAuthServer::request_del_user(client, username);
+}
+
+void PipeServer::request_auth(Client &client, const std::string &username, const std::string &password) {
+	if (dynamic_cast<PipedClient*>(&client))
+		return send(client, error("AUTH command not allowed in PIPE request"));
+	SerialAuthServer::request_auth(client, username, password);
+}
+
+void PipeServer::request_change_password(Client &client, const std::string &new_password) {
+	if (dynamic_cast<PipedClient*>(&client))
+		return send(client, error("CHANGE_PASSWORD command not allowed in PIPE request"));
+	SerialAuthServer::request_change_password(client, new_password);
+}
+
 } // namespace vanity
--- vanity/pipe_server.h
+++ vanity/pipe_server.h
@@ -5,7 +5,7 @@
 #ifndef VANITY_PIPE_SERVER_H
 #define VANITY_PIPE_SERVER_H
 
-#include "request_server.h"
+#include "auth/serial_auth_server.h"
 #include "socket/socket_server.h"
 
 namespace vanity {
@@ -13,11 +13,26 @@ namespace vanity {
 /*
  * A PipeServer is a server that handles PIPE requests
  */
-class PipeServer : public virtual RequestServer, public virtual SocketServer
+class PipeServer : public virtual SerialAuthServer, public virtual SocketServer
 {
 public:
 	// a pipe request was received from a client
 	void request_pipe(Client& client, const std::string& msg, size_t& pos) override;
+
+	// an add_user request was received from a client
+	void request_add_user(Client& client, const std::string& username, const std::string& password) override;
+
+	// an edit_user request was received from a client
+	void request_edit_user(Client& client, const std::string& username, client_auth auth_level) override;
+
+	// a del_user request was received from a client
+	void request_del_user(Client& client, const std::string& username) override;
+
+	// an auth request was received from a client
+	void request_auth(Client& client, const std::string& username, const std::string& password) override;
+
+	// a change_password request was received from a client
+	void request_change_password(Client& client, const std::string& new_password) override;
 };
 
 } // namespace vanity
