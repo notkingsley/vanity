aafc80d5ab8b1333f27bf6e6b50ae6f30de14c88
kingsli <kingoyelabi@gmail.com>
2024-01-21 14:41:17 +0100

    add RepeatEventServer to automate simple repeated events

---
+++ vanity/repeat_event_server.cpp
@@ -0,0 +1,62 @@
+//
+// Created by kingsli on 1/21/24.
+//
+
+#include "repeat_event_server.h"
+
+namespace vanity {
+
+void RepeatEventServer::repeat_event_every(server_event event, microseconds interval) {
+	m_repeat_events.push({now() + interval, interval, event});
+}
+
+void RepeatEventServer::repeat_event_every(server_event event, long interval) {
+	repeat_event_every(event, microseconds(interval));
+}
+
+void RepeatEventServer::repeat_loop() {
+	if (m_repeat_events.empty())
+		return;
+
+	reset_events();
+	while (true) {
+		auto [next, interval, event] = m_repeat_events.top();
+		m_repeat_events.pop();
+
+		auto sleep_time = next - now();
+		if (sleep_time.count() < 0)
+			sleep_time = microseconds(0);
+
+		using std::chrono::duration_cast;
+		if (m_stopped.wait(duration_cast<microseconds>(sleep_time).count()))
+			return;
+
+		push_event(event);
+		m_repeat_events.emplace(next + interval, interval, event);
+	}
+}
+
+void RepeatEventServer::start() {
+	m_stopped.clear();
+	m_repeat_loop_thread = std::thread(&RepeatEventServer::repeat_loop, this);
+}
+
+void RepeatEventServer::stop() {
+	m_stopped.set();
+	m_repeat_loop_thread.join();
+}
+
+void RepeatEventServer::reset_events() {
+	std::priority_queue<repeated_event> new_events;
+	auto now = steady_clock::now();
+
+	while (not m_repeat_events.empty()) {
+		auto [next, interval, event] = m_repeat_events.top();
+		m_repeat_events.pop();
+		new_events.emplace(now + interval, interval, event);
+	}
+
+	m_repeat_events.swap(new_events);
+}
+
+} // namespace vanity
---
+++ vanity/repeat_event_server.h
@@ -0,0 +1,70 @@
+//
+// Created by kingsli on 1/21/24.
+//
+
+#ifndef VANITY_REPEAT_EVENT_SERVER_H
+#define VANITY_REPEAT_EVENT_SERVER_H
+
+#include "event_server.h"
+#include "utils/event.h"
+
+namespace vanity {
+
+/*
+ * A RepeatEventServer allows an event to be
+ * repeatedly pushed after some constant time interval
+ */
+class RepeatEventServer : public virtual EventServer
+{
+private:
+	using steady_clock = std::chrono::steady_clock;
+	using microseconds = std::chrono::microseconds;
+
+	// a repeated event
+	struct repeated_event {
+		steady_clock::time_point next;
+		microseconds interval;
+		server_event event;
+
+		bool operator<(const repeated_event& other) const {
+			return next > other.next;
+		}
+	};
+
+	// registered repeat events
+	std::priority_queue<repeated_event> m_repeat_events;
+
+	// whether the repeat events have been stopped
+	event m_stopped;
+
+	// the repeat_loop thread
+	std::thread m_repeat_loop_thread;
+
+	// loop to repeat events
+	void repeat_loop();
+
+	// reset next time_points for all events
+	void reset_events();
+
+	// get the current time
+	static steady_clock::time_point now(){
+		return steady_clock::now();
+	};
+
+protected:
+	// register an event to be repeated every interval microseconds
+	void repeat_event_every(server_event event, microseconds interval);
+
+	// register an event to be repeated every interval microseconds
+	void repeat_event_every(server_event event, long interval);
+
+public:
+	// start repeating events
+	void start();
+
+	// stop repeating events
+	void stop();
+};
+
+} // namespace vanity
+#endif //VANITY_REPEAT_EVENT_SERVER_H
