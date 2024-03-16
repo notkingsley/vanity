197b0990fee27dce040a3b844138fc964e14129d
kingsli <kingoyelabi@gmail.com>
2024-03-16 20:44:56 +0100

    correct deep_purge() behaviour by collecting expired keys first

--- vanity/db/db/expiry_database.cpp
+++ vanity/db/db/expiry_database.cpp
@@ -21,11 +21,14 @@ void ExpiryDatabase::expire(const key_type& key) {
 }
 
 void ExpiryDatabase::_do_expire(const key_type &key) {
-	clear_expiry(key);
 	m_data.erase(key);
+	clear_expiry(key);
 }
 
 bool ExpiryDatabase::is_expired(const key_type &key) {
+	if (not m_expiry_enabled)
+		return false;
+
 	if (not m_expiry_times.contains(key))
 		return false;
 
@@ -33,9 +36,6 @@ bool ExpiryDatabase::is_expired(const key_type &key) {
 }
 
 bool ExpiryDatabase::erase_if_expired(const key_type &key) {
-	if (not m_expiry_enabled)
-		return false;
-
 	if (not is_expired(key))
 		return false;
 
@@ -68,34 +68,38 @@ void ExpiryDatabase::shallow_purge() {
 	static std::random_device rd;
 	static std::mt19937 gen(rd());
 
-	while (true) {
-		std::uniform_int_distribution<size_t> dis(0, m_expiry_times.size() - 1);
-		size_t sample_size = std::min<size_t>(M_MAX_SAMPLE_SIZE, m_expiry_times.size());
+	while (not m_expiry_times.empty())
+	{
+		auto sample_size = std::min<size_t>(M_MAX_SAMPLE_SIZE, m_expiry_times.size());
 		auto all_keys = std::views::keys(m_expiry_times);
-
 		std::vector<key_type> sampled_keys;
 		sampled_keys.reserve(sample_size);
 
-		sample(all_keys.begin(), all_keys.end(), back_inserter(sampled_keys), sample_size, gen);
-		size_t expired_count = std::count_if(
-			sampled_keys.begin(),
-			sampled_keys.end(),
-			[this](const auto& key) {
-				return erase_if_expired(key);
-			}
+		sample(
+			all_keys.begin(),
+			all_keys.end(),
+			back_inserter(sampled_keys),
+			sample_size,
+			gen
 		);
+		size_t expired_count = 0;
+		for (auto& key: sampled_keys)
+			if (erase_if_expired(key))
+				expired_count += 1;
 
 		if (expired_count / sample_size < M_MIN_EXPIRED_PERCENTAGE)
-			break;
-
-		if (m_expiry_times.empty())
 			break;
 	}
 }
 
 void ExpiryDatabase::deep_purge() {
-	for (auto& [key, _] : m_expiry_times)
-		erase_if_expired(key);
+	std::vector<key_type> expired_keys;
+	for (auto& key: std::views::keys(m_expiry_times))
+		if (is_expired(key))
+			expired_keys.push_back(key);
+
+	for (auto& key: expired_keys)
+		expire(key);
 }
 
 void ExpiryDatabase::on_expire(callback_t callback) {
