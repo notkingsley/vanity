3d3410264232141ba43d2f2dbf5d6a2b1930e785
kingsli <kingoyelabi@gmail.com>
2024-04-29 02:09:48 +0100

    add PostRequestServer to handle PostRequests

---
+++ vanity/request/post_request_server.cpp
@@ -0,0 +1,26 @@
+//
+// Created by kingsli on 4/29/24.
+//
+
+#include "post_request_server.h"
+
+namespace vanity {
+
+void PostRequestServer::handle_post_request(PostRequest &request, Client &client) {
+	auto id = request.get<object_t::INT>();
+	auto op = request.get_op();
+	Context ctx {id, client};
+
+	switch (op) {
+		case peer_op_t::PING: {
+			post_request_ping(ctx);
+			break;
+		}
+	}
+}
+
+void PostRequestServer::post_request_ping(Context& ctx) {
+	// TODO: send pong
+}
+
+} // namespace vanity
---
+++ vanity/request/post_request_server.h
@@ -0,0 +1,39 @@
+//
+// Created by kingsli on 4/29/24.
+//
+
+#ifndef VANITY_POST_REQUEST_SERVER_H
+#define VANITY_POST_REQUEST_SERVER_H
+
+#include "client/client.h"
+#include "post_request.h"
+
+namespace vanity {
+
+/*
+ * A PostRequestServer is a server that handles PostRequests
+ * from peers
+ */
+class PostRequestServer
+{
+protected:
+	// context passed to handlers
+	struct Context {
+		// the type of the identifier of a message
+		using msg_id_t = int64_t;
+
+		msg_id_t msg_id;
+		Client &client;
+	};
+
+public:
+	// handle a PostRequest
+	void handle_post_request(PostRequest& request, Client &client);
+
+	// a ping request was received from a peer
+	void post_request_ping(Context& ctx);
+};
+
+} // namespace vanity
+
+#endif //VANITY_POST_REQUEST_SERVER_H
