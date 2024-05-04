ec102bd465d62006adb787fc6dd96c06bb689128
kingsli <kingoyelabi@gmail.com>
2024-05-04 02:44:37 +0100

    merge PostRequestServer and ReplyRequestServer into PeerRequestServer

--- vanity/request/peer_request_server.cpp
+++ vanity/request/peer_request_server.cpp
@@ -3,6 +3,9 @@
 //
 
 #include "peer_request_server.h"
+#include "post_request.h"
+#include "reply_request.h"
+
 
 namespace vanity {
 
@@ -12,14 +15,75 @@ void PeerRequestServer::handle_peer(const std::string &msg, Client &client) {
 
 	switch (request_t) {
 		case peer_request_t::POST: {
-			PostRequest post_request{request};
-			handle_post_request(post_request, client);
+			handle_post_request(request, client);
 			break;
 		}
 		case peer_request_t::REPLY: {
-			ReplyRequest reply_request{request};
-			handle_reply_request(reply_request, client);
+			handle_reply_request(request, client);
 			break;
+		}
+	}
+}
+
+void PeerRequestServer::post_request_ping(Context& ctx) {
+	reply(ctx, PONG);
+}
+
+void PeerRequestServer::reply_request_ping(Client &client) { }
+
+void PeerRequestServer::handle_post_request(PeerRequest& peer_request, Client &client) {
+	using enum object_t;
+
+	PostRequest request{peer_request};
+	auto id = request.get<INT>();
+	auto op = request.get_op();
+	Context ctx {id, client};
+	bool end = true;
+
+	switch (op) {
+		case peer_op_t::PING: {
+			post_request_ping(ctx);
+			break;
+		}
+
+		case peer_op_t::PEER_AUTH: {
+			auto [key, addr] = request.get_exact<STR, STR>(end);
+			post_request_peer_auth(ctx, key, addr);
+			break;
+		}
+
+		case peer_op_t::MAX_OP: {
+			// TODO: report peer
+		}
+	}
+}
+
+void PeerRequestServer::handle_reply_request(PeerRequest& peer_request, Client &client) {
+	using enum object_t;
+
+	ReplyRequest request{peer_request};
+	auto id = request.get<INT>();
+	auto op = expected_op(id);
+	bool end = true;
+
+	if (not op) {
+		// TODO: report peer
+	}
+
+	switch (*op) {
+		case peer_op_t::PING: {
+			reply_request_ping(client);
+			break;
+		}
+
+		case peer_op_t::PEER_AUTH: {
+			auto data = request.get_exact<STR>(end);
+			reply_request_peer_auth(client, data);
+			break;
+		}
+
+		case peer_op_t::MAX_OP: {
+			// TODO: report peer
 		}
 	}
 }
--- vanity/request/peer_request_server.h
+++ vanity/request/peer_request_server.h
@@ -6,19 +6,42 @@
 #define VANITY_PEER_REQUEST_SERVER_H
 
 #include "peer_request.h"
-#include "post_request_server.h"
-#include "reply_request_server.h"
+#include "response/post_message_server.h"
+#include "response/reply_message_server.h"
 
 namespace vanity {
 
 /*
  * A PeerRequestServer is a server that handles messages from peers
  */
-class PeerRequestServer : public virtual PostRequestServer, public virtual ReplyRequestServer
+class PeerRequestServer : public virtual PostMessageServer, public virtual ReplyMessageServer
 {
-public:
+private:
+	// string for a PONG reply
+	static constexpr auto PONG = "PONG";
+
+	// handle a POST request
+	void handle_post_request(PeerRequest& peer_request, Client &client);
+
+	// handle a REPLY request
+	void handle_reply_request(PeerRequest& peer_request, Client &client);
+
+protected:
 	// handle a peer's request
 	void handle_peer(const std::string &msg, Client &client);
+
+public:
+	// a ping request was received from a peer
+	virtual void post_request_ping(Context& ctx);
+
+	// a reply to a ping request was received from a peer
+	virtual void reply_request_ping(Client &client);
+
+	// a peer_auth request was received from a peer
+	virtual void post_request_peer_auth(Context& ctx, const std::string& key, const std::string& addr) = 0;
+
+	// a reply to a peer_auth request was received from a peer
+	virtual void reply_request_peer_auth(Client &client, const std::string& data) = 0;
 };
 
 } // namespace vanity
