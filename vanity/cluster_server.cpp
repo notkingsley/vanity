281701f691c976a3bd9f748803da183505a8c57f
kingsli <kingoyelabi@gmail.com>
2024-05-04 17:45:52 +0100

    block CLUSTER_JOIN until peer reply and handle concurrent CLUSTER_JOIN requests

--- vanity/cluster_server.cpp
+++ vanity/cluster_server.cpp
@@ -16,10 +16,24 @@ void ClusterServer::handle(const std::string &msg, Client &client) {
 }
 
 bool ClusterServer::validate_cluster_key(const std::string &key) {
+	std::lock_guard lock{m_cluster_key_mutex};
 	if (not m_cluster_key)
 		return false;
 
 	return key == *m_cluster_key;
+}
+
+void ClusterServer::set_cluster_key(const std::string &key) {
+	std::lock_guard lock{m_cluster_key_mutex};
+	clear_auth_applications();
+	m_cluster_key = key;
+}
+
+void ClusterServer::cluster_join(Client& client, const std::string &key, const std::string &host, uint16_t port) {
+	auto& peer = connect(host, port);
+	auto id = post_plain(peer, peer_op_t::PEER_AUTH, key, get_own_address());
+	register_peer(peer, make_address(host, port));
+	add_auth_application(id, key, client);
 }
 
 std::string ClusterServer::get_own_address() const {
@@ -32,18 +46,15 @@ TcpClient &ClusterServer::connect(const std::string &host, uint16_t port) {
 }
 
 void ClusterServer::request_cluster_join(Client &client, const std::string& key, const std::string &host, uint16_t port) {
+	std::lock_guard lock{m_cluster_key_mutex};
 	if (m_cluster_key)
 		return send(client, error("already in a cluster"));
 
-	auto& peer = connect(host, port);
-	post_plain(peer, peer_op_t::PEER_AUTH, key, get_own_address());
-	register_peer(peer, make_address(host, port));
-
-	m_cluster_key = key;
-	send(client, ok());
+	cluster_join(client, key, host, port);
 }
 
 void ClusterServer::request_cluster_leave(Client &client) {
+	std::lock_guard lock{m_cluster_key_mutex};
 	if (not m_cluster_key)
 		return send(client, error("not in a cluster"));
 
@@ -53,6 +64,7 @@ void ClusterServer::request_cluster_leave(Client &client) {
 }
 
 void ClusterServer::request_cluster_key(Client &client) {
+	std::lock_guard lock{m_cluster_key_mutex};
 	if (m_cluster_key)
 		send(client, ok(*m_cluster_key));
 	else
@@ -60,13 +72,14 @@ void ClusterServer::request_cluster_key(Client &client) {
 }
 
 void ClusterServer::request_cluster_new(Client &client, const std::string &key) {
+	std::lock_guard lock{m_cluster_key_mutex};
 	if (m_cluster_key)
 		return send(client, error("already in a cluster"));
 
 	if (key.size() < M_MIN_CLUSTER_KEY_LEN)
 		return send(client, error("key is too short"));
 
-	m_cluster_key = key;
+	set_cluster_key(key);
 	send(client, ok(*m_cluster_key));
 }
 
@@ -88,13 +101,19 @@ void ClusterServer::post_request_peer_auth(Context&, const std::string&, const s
 }
 
 void ClusterServer::reply_request_peer_auth(Context& ctx, const std::string &data) {
+	auto pending = pop_auth_application(ctx.msg_id);
+	if (not pending)
+		return remove_peer(ctx.client);
+
 	if (data == "OK") {
 		global_log("Connected to peer");
+		set_cluster_key(pending->key);
+		send(pending->client, ok());
 		post(ctx.client, peer_op_t::PING);
 	}
 	else if (data == "DENIED") {
 		global_log("Peer denied connection");
-		m_cluster_key.reset();
+		send(pending->client, error("peer denied connection"));
 		remove_peer(ctx.client);
 	}
 	else {
@@ -104,6 +123,31 @@ void ClusterServer::reply_request_peer_auth(Context& ctx, const std::string &dat
 
 std::string ClusterServer::make_address(const std::string &host, uint16_t port) {
 	return host + ":" + std::to_string(port);
+}
+
+void ClusterServer::add_auth_application(msg_id_t id, const std::string &key, Client &client) {
+	std::lock_guard lock{m_pending_peer_auths_mutex};
+	m_pending_peer_auths.emplace(id, PendingPeerAuth{key, client});
+}
+
+auto ClusterServer::pop_auth_application(msg_id_t id) -> std::optional<PendingPeerAuth> {
+	std::lock_guard lock{m_pending_peer_auths_mutex};
+	auto it = m_pending_peer_auths.find(id);
+
+	if (not m_pending_peer_auths.contains(id))
+		return std::nullopt;
+
+	auto pending = it->second;
+	m_pending_peer_auths.erase(it);
+	return pending;
+}
+
+void ClusterServer::clear_auth_applications() {
+	std::lock_guard lock{m_pending_peer_auths_mutex};
+	for (auto& [_, pending] : m_pending_peer_auths)
+		send(pending.client, error("already in a cluster"));
+
+	m_pending_peer_auths.clear();
 }
 
 } // namespace vanity
--- vanity/cluster_server.h
+++ vanity/cluster_server.h
@@ -19,11 +19,29 @@ namespace vanity {
 class ClusterServer : public virtual BindServer, public virtual PeerRequestServer, public virtual PeerServer
 {
 private:
+	// a pending application we've sent as a peer_auth request
+	struct PendingPeerAuth {
+		// the key of the application
+		std::string key;
+
+		// the client that sent the request
+		Client& client;
+	};
+
 	// the minimum length of a cluster key
 	static constexpr auto M_MIN_CLUSTER_KEY_LEN = 12;
 
+	// all pending peer_auth requests
+	std::unordered_map<msg_id_t, PendingPeerAuth> m_pending_peer_auths;
+
+	// the mutex for the pending peer_auth requests
+	std::mutex m_pending_peer_auths_mutex;
+
 	// the key of the cluster, if any
 	std::optional<std::string> m_cluster_key;
+
+	// the mutex for the cluster key
+	std::mutex m_cluster_key_mutex;
 
 	// make an address by joining a host and a port into a string
 	static std::string make_address(const std::string& host, uint16_t port);
@@ -32,11 +50,26 @@ private:
 	// returns true if the key is valid, false otherwise
 	bool validate_cluster_key(const std::string& key);
 
+	// sets the cluster key
+	void set_cluster_key(const std::string& key);
+
+	// connect to, and send a peer_auth request to a remote server
+	void cluster_join(Client& client, const std::string& key, const std::string& host, uint16_t port);
+
 	// get this server's address
 	std::string get_own_address() const;
 
 	// connect to a remote server
 	TcpClient& connect(const std::string &host, uint16_t port);
+
+	// add a peer_auth request to the pending requests
+	void add_auth_application(msg_id_t id, const std::string& key, Client& client);
+
+	// pop a pending peer_auth request
+	std::optional<PendingPeerAuth> pop_auth_application(msg_id_t id);
+
+	// clear all pending peer_auth requests
+	void clear_auth_applications();
 
 public:
 	// a message was received from a client
