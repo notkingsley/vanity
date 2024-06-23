0a662b63798c588b35a097aa1518e353fa6270f1
kingsli <kingoyelabi@gmail.com>
2024-06-23 03:02:29 +0100

    update NodeHandle.cluster_id with responses

--- client/node_handle.py
+++ client/node_handle.py
@@ -14,6 +14,7 @@ class NodeHandle:
         host: str | None = None,
         ports: list[int] | None = None,
         cluster_port: int | None = None,
+        cluster_id: str | None = None,
         server_args: dict | None = None,
         client_args: dict[str, str | None] | None = None,
     ):
@@ -22,6 +23,7 @@ class NodeHandle:
         self.cluster_port = cluster_port or self.get_free_port()
         self.client_args = client_args or self.default_client_args()
         self.server_args = server_args or {}
+        self.cluster_id = cluster_id
         self.server = ServerHandle(
             host=self.host,
             ports=self.ports,
@@ -57,23 +59,31 @@ class NodeHandle:
         """
         return Client(self.host, self.ports[0], **self.client_args)
 
-    def new_cluster(self, cluster_key: str):
+    def new_cluster(self, cluster_key: str, id: str | None = None):
         """
         Create a new cluster on the node.
         """
+        id = id or self.cluster_id
         with self.own_client() as client:
-            response = client.cluster_new(cluster_key)
+            response = client.cluster_new(cluster_key, id)
             if not response.is_ok():
                 raise RuntimeError(f"Failed to create new cluster: {response.value}")
 
-    def join_cluster(self, cluster_key: str, host: str, port: int):
+            self.cluster_id: str = response.value
+
+    def join_cluster(
+        self, cluster_key: str, host: str, port: int, id: str | None = None
+    ):
         """
         Join a cluster on the node.
         """
+        id = id or self.cluster_id
         with self.own_client() as client:
-            response = client.cluster_join(cluster_key, host, port)
+            response = client.cluster_join(cluster_key, host, port, id)
             if not response.is_ok():
                 raise RuntimeError(f"Failed to join cluster: {response.value}")
+
+            self.cluster_id: str = response.value
 
     def leave_cluster(self):
         """
