2f1722788590ade528ec165448319ce7d860bd3b
kingsli <kingoyelabi@gmail.com>
2024-09-02 02:01:03 +0100

    remove behaviour and state from request operations checks

--- vanity/request/request_server.cpp
+++ vanity/request/request_server.cpp
@@ -24,7 +24,7 @@ void RequestServer::do_handle(Client &client, Request &request, size_t len) {
 bool RequestServer::do_handle_single(Client &client, Request& request, bool end) {
 	try
 	{
-		return dispatch_by_behavior(client, request, end);
+		return dispatch_or_refuse(client, request, end);
 	}
 	catch (const InvalidRequest& e)
 	{
@@ -51,53 +51,7 @@ bool RequestServer::do_handle_single(Client &client, Request& request, bool end)
 	}
 }
 
-bool RequestServer::dispatch_by_behavior(Client &client, Request& request, bool end) {
-	switch (behaviour(request.peek_operation(), session_state(client))) {
-		case behaviour_t::DEFAULT:
-		{
-			return dispatch_default_behavior(client, request, end);
-		}
-		case behaviour_t::NOT_PERMITTED:
-		{
-			return dispatch_not_permitted_behavior(client, request, end);
-		}
-		case behaviour_t::CONTEXTUAL:
-		{
-			return dispatch_contextual_behavior(client, request, end);
-		}
-		default:
-		{
-			throw std::runtime_error("invalid behaviour");
-		}
-	}
-}
-
-bool RequestServer::dispatch_default_behavior(Client& client, Request& request, bool end) {
-	return dispatch_normal_context(client, request, end);
-}
-
-bool RequestServer::dispatch_not_permitted_behavior(Client& client, Request& request, bool end) {
-	return refuse_with_response(client, request, bad_state(), end);
-}
-
-bool RequestServer::dispatch_contextual_behavior(Client& client, Request& request, bool end) {
-	switch (session_state(client)) {
-		case user_data_t::	conn_state::NORMAL:
-		{
-			return dispatch_normal_context(client, request, end);
-		}
-		case user_data_t::conn_state::TRANSACTION:
-		{
-			return dispatch_transaction_context(client, request, end);
-		}
-		default:
-		{
-			throw std::runtime_error("invalid state");
-		}
-	}
-}
-
-bool RequestServer::dispatch_normal_context(Client &client, Request& request, bool end) {
+bool RequestServer::dispatch_or_refuse(Client &client, Request& request, bool end) {
 	if (not client.has_perm(request.peek_operation()))
 		return refuse_with_response(client, request, denied(), end);
 
--- vanity/request/request_server.h
+++ vanity/request/request_server.h
@@ -44,6 +44,7 @@ protected:
 	// behavior is undefined if one of the requests couldn't be dispatched
 	void do_handle(Client& client, Request& request, size_t len);
 
+private:
 	// extract the data from a request and dispatch it to the appropriate handler
 	// client: the client that sent the message
 	// request: the request to extract data from
@@ -51,33 +52,8 @@ protected:
 	// return true if the request was extracted and dispatched successfully, false otherwise
 	bool do_handle_single(Client& client, Request& request, bool end);
 
-	// dispatch a request based on the client's behavior
-	// this selects the correct handler based on the client's current state
-	// effectively same as do_handle, but doesn't catch errors or guarantee a response will be sent
-	bool dispatch_by_behavior(Client& client, Request& request, bool end);
-
-	// dispatch a request in the default behavior
-	// this is the default behavior for a client, and is used when the client is not in any special state
-	// or when the request is not affected by the client's state/behavior
-	// returns true if the request was dispatched successfully, false otherwise
-	bool dispatch_default_behavior(Client& client, Request& request, bool end);
-
-	// dispatch a request not permitted in the current state
-	// this is called when a request is not permitted in the current state
-	// and is distinct from the request not being permitted due to the client's auth level
-	// returns true if the request was dispatched successfully, false otherwise
-	bool dispatch_not_permitted_behavior(Client& client, Request& request, bool end);
-
-	// dispatch a request in a contextual behavior
-	// this is called when a request is permitted in the current state, but is affected by the client's state/behavior
-	// returns true if the request was dispatched successfully, false otherwise
-	bool dispatch_contextual_behavior(Client& client, Request& request, bool end);
-
-	// dispatch a request in a normal context
-	bool dispatch_normal_context(Client& client, Request& request, bool end);
-
-	// dispatch a request in a transaction context
-	virtual bool dispatch_transaction_context(Client& client, Request& request, bool end) = 0;
+	// check for permissions and dispatch a request or refuse it
+	bool dispatch_or_refuse(Client& client, Request& request, bool end);
 
 	// send a response to the client, calling dry_dispatch if necessary
 	// always returns false
